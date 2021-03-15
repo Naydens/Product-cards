4a0148d56ec5f0fdfd21fd3b35b227b1e8aec985
Nadya <batiskaf2017@gmail.com>
2021-03-15 11:02:35 +0200

    functions plus and minus. show quantity in small round in cart. function calculate total cart items

--- cards.js
+++ cards.js
@@ -1,8 +1,61 @@
-const minus = document.getElementsByClassName("minus");
-console.log(minus);
-for(let i=0;i<minus.length;i++){
-    const elem = minus[i];
-    elem.onclick = function(){
-        alert("jsdfg");
+const minusQuantity = document.querySelectorAll(".js-minus-quantity");
+
+const plusQuantity = document.querySelectorAll(".js-plus-quantity");
+
+const cartQuantity = document.querySelector(".js-cart-quantity");
+
+const addToCartButtons = document.querySelectorAll(".js-add-to-cart");
+
+const cart = {};
+
+function calculateTotalCartItems() {
+  let total = 0;
+  for (let key in cart) {
+    total += cart[key];
+  }
+  return total;
+}
+
+function addToCart(e) {
+  const productId = e.target.getAttribute("data-product-id");
+  const quantity = parseInt(
+    e.target.parentNode.querySelector(".js-cart-count").value
+  );
+
+  if (cart[productId] === undefined) {
+    cart[productId] = quantity;
+  } else {
+    cart[productId] += quantity;
+  }
+
+  cartQuantity.innerHTML = calculateTotalCartItems();
+}
+
+for (let i = 0; i < addToCartButtons.length; i++) {
+  const elem = addToCartButtons[i];
+  elem.addEventListener("click", addToCart);
+}
+
+function minus(e) {
+  const input = e.target.nextSibling.nextSibling;
+  if (input.value > 1) {
+      input.value--;
+  }
+}
+
+for (let i = 0; i < minusQuantity.length; i++) {
+  const elem = minusQuantity[i];
+  elem.addEventListener("click", minus);
+}
+
+function plus (e){
+    const input = e.target.previousSibling.previousSibling;
+    if(input.value<input.size){
+        input.value++;
     }
+}
+
+for (let i = 0; i < plusQuantity.length; i++) {
+    const elem = plusQuantity[i];
+    elem.addEventListener("click", plus);
   }
